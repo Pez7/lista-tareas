4dc3a0292350fed2cf9e84660e1bf75d06623efe
Maria Paz <maria.pazrojasc@gmail.com>
2017-04-27 13:25:07 -0300

    proyecto final

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+var arr = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  },
+ ]
+
+var info = document.getElementById("tarea");
+
+function nuevaTarea(){
+	var arr = [];
+	tarea = document.getElementById("inputTarea").value;
+	var nueva = info.innerHTML += '<li>' + tarea + '</li><br>';
+
+	arr.push(nueva);
+}
+
+
+function agregarTarea(){
+
+	var recorrido = arr.forEach(function(e){
+	info.innerHTML += '<li>'+ e.title +'</li><br>';
+
+});
+}
+
